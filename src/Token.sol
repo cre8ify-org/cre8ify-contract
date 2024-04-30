11cb31aeea0c108a274be9bb8de81ebea20de233
mubarakOke <okeolamubarak18@gmail.com>
2024-04-30 13:17:52 +0100

    updated ccp.sol, subscription, token

--- src/Token.sol
+++ src/Token.sol
@@ -1,18 +1,11 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
-    uint256 public subscriptionAmount;
-
-   constructor(string memory name, string memory symbol, uint256 initialSupply, uint256 _subscriptionAmount) ERC20(name, symbol) {
-    _mint(msg.sender, 10000000000000000000); // Increase initial supply to 10 trillion tokens
-    subscriptionAmount = _subscriptionAmount;
-}
-
-    function subscribe() external {
-        require(balanceOf(msg.sender) >= subscriptionAmount, "Insufficient balance");
-        _burn(msg.sender, subscriptionAmount);
+   constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        _mint(msg.sender, 10000000000000000000); 
     }
 
     function mint(address account, uint256 amount) external {

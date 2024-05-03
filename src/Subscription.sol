7a51b328889e57f3073bdd4fe4d1c9ce1d15333d
mubarakOke <okeolamubarak18@gmail.com>
2024-05-04 00:58:46 +0100

    umpdate CCP and Subscription contract

--- src/Subscription.sol
+++ src/Subscription.sol
@@ -7,12 +7,15 @@ contract Subscription {
     IERC20 public token;
     mapping(address => bool) public isSubscribed;
     mapping(address => uint256) public subscriptionExpiry;
+    mapping(address => uint256) public subscriptionType;
 
-    event Subscribed(address indexed user, uint256 expiry);
+    event Subscribed(address indexed user, uint256 expiry, uint256 subsciptionPackage);
     event SubscriptionExtended(address indexed user, uint256 expiry);
 
     uint256 subscriptionBaseFee = 1e2;
-    uint256 discountRate = 25;
+    uint256 discountRate3 = 7;
+    uint256 discountRate6 = 15;
+    uint256 discountRate12 = 25;
 
     constructor(address _tokenAddress) {
         token = IERC20(_tokenAddress);
@@ -25,19 +28,15 @@ contract Subscription {
     }
 
     // Subscribe to platform using tokens
-    function subscribe(uint256 _days) public {
+    function subscribeOneMonth() public {
         require(!isSubscribed[msg.sender] && !(subscriptionExpiry[msg.sender] >= block.timestamp), "User is already subscribed");
 
-        uint256 subDays = _days * 1 days;
+        uint256 subDays = 30 * 1 days;
         uint256 baseAmount = subDays * subscriptionBaseFee;
 
-        uint256 discount = 0;
-        if (_days >= 365 days) {
-            discount = baseAmount * discountRate / 100;
-        }
+        uint256 amount = baseAmount;
 
-        uint256 amount = baseAmount - discount;
-
+        subscriptionType[msg.sender] = 1;
         // Update subscription details
         isSubscribed[msg.sender] = true;
 
@@ -46,7 +45,75 @@ contract Subscription {
 
         subscriptionExpiry[msg.sender] = block.timestamp + subDays;
 
-        emit Subscribed(msg.sender, subscriptionExpiry[msg.sender]);
+        emit Subscribed(msg.sender, subscriptionExpiry[msg.sender], 1);
+    }
+
+    function subscribeThreeMonths() public {
+        require(!isSubscribed[msg.sender] && !(subscriptionExpiry[msg.sender] >= block.timestamp), "User is already subscribed");
+
+        uint256 subDays = 90 * 1 days;
+        uint256 baseAmount = subDays * subscriptionBaseFee;
+
+        uint256 discount = baseAmount * discountRate3 / 100;
+
+        uint256 amount = baseAmount - discount;
+
+        // Update subscription details
+        isSubscribed[msg.sender] = true;
+        subscriptionType[msg.sender] = 2;
+
+        // Transfer tokens from user to contract
+        token.transferFrom(msg.sender, address(this), amount);
+
+        subscriptionExpiry[msg.sender] = block.timestamp + subDays;
+
+        emit Subscribed(msg.sender, subscriptionExpiry[msg.sender], 2);
+    }
+
+    function subscribeSixMonths() public {
+        require(!isSubscribed[msg.sender] && !(subscriptionExpiry[msg.sender] >= block.timestamp), "User is already subscribed");
+
+        uint256 subDays = 180 * 1 days;
+        uint256 baseAmount = subDays * subscriptionBaseFee;
+
+        uint256 discount = baseAmount * discountRate6 / 100;
+
+        uint256 amount = baseAmount - discount;
+
+        // Update subscription details
+        isSubscribed[msg.sender] = true;
+        subscriptionType[msg.sender] = 3;
+
+        // Transfer tokens from user to contract
+        token.transferFrom(msg.sender, address(this), amount);
+
+        subscriptionExpiry[msg.sender] = block.timestamp + subDays;
+
+        emit Subscribed(msg.sender, subscriptionExpiry[msg.sender], 3);
+    }
+
+
+     function subscribe12Months() public {
+        require(!isSubscribed[msg.sender] && !(subscriptionExpiry[msg.sender] >= block.timestamp), "User is already subscribed");
+
+        uint256 subDays = 365  * 1 days;
+        uint256 baseAmount = subDays * subscriptionBaseFee;
+
+        uint256 discount = baseAmount * discountRate12 / 100;
+
+        uint256 amount = baseAmount - discount;
+
+        // Update subscription details
+        isSubscribed[msg.sender] = true;
+
+        subscriptionType[msg.sender] = 3;
+
+        // Transfer tokens from user to contract
+        token.transferFrom(msg.sender, address(this), amount);
+
+        subscriptionExpiry[msg.sender] = block.timestamp + subDays;
+
+        emit Subscribed(msg.sender, subscriptionExpiry[msg.sender], 3);
     }
 
     // Check if user is subscribed
@@ -58,26 +125,11 @@ contract Subscription {
         }
     }
 
-    // Extend subscription duration
-    function extendSubscription(uint256 _days) public onlySubscribed {
-        require(isSubscribed[msg.sender] && subscriptionExpiry[msg.sender] >= block.timestamp, "User is not subscribed");
-
-        uint256 subDays = _days * 1 days;
-        uint256 baseAmount = subDays * subscriptionBaseFee;
-
-        uint256 discount = 0;
-        if (_days >= 365 days) {
-            discount = baseAmount * discountRate / 100;
+    function getSubscriptionPackage(address _user) public view returns (uint256) {
+        if (isSubscribed[_user] && subscriptionExpiry[_user] >= block.timestamp) {
+            return subscriptionType[msg.sender];
+        } else {
+            return 0;
         }
-
-        uint256 amount = baseAmount - discount;
-
-        // Transfer tokens from user to contract
-        token.transferFrom(msg.sender, address(this), amount);
-
-        // Extend subscription expiry
-        subscriptionExpiry[msg.sender] += subDays;
-
-        emit SubscriptionExtended(msg.sender, subscriptionExpiry[msg.sender]);
     }
 }
